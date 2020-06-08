c455d22598bc9c78b2c38bd683b9cd2690b8da2c
Olga Botez <sungirl990@gmail.com>
2020-06-08 16:56:17 +0100

    added a slider menu to the slider section

---
+++ slider.js
@@ -0,0 +1,22 @@
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const items = document.querySelector("#items");
+
+
+right.addEventListener("click", function(e) {
+  loop("right", e);
+});
+ 
+left.addEventListener("click", function(e) {
+  loop("left", e);
+});
+
+function loop(direction, e) {
+  e.preventDefault();
+  if (direction === "right") {
+    items.appendChild(items.firstElementChild);
+  } else {
+    items.insertBefore(items.lastElementChild, items.firstElementChild);
+  }
+}
+
